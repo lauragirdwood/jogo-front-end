177a31eb22b648aed9a207a2c5b54eea2d57d0d7
Laura Girdwood <azevedoemello@gmail.com>
2019-07-25 14:28:36 -0300

    add usuario service

--- nosso-angular/src/app/usuario.service.ts
+++ nosso-angular/src/app/usuario.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from '../../node_modules/rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class UsuarioService {
 
-  constructor() { }
+  private baseUrl = "http://www.mocky.io/v2/5d39d0ad2f000049006ebe71"
+
+  constructor(private http: HttpClient) { }
+
+  // READ do CRUD
+  getUsuarios(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);  // isso aqui são crases!
+  }
+
+  getUsuario(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);  // isso aqui são crases!
+  }
+
+  // CREATE do CRUD
+  createUsuario(usuario: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, usuario);  // isso aqui são crases!
+  }
+
+  // UPDATE do CRUD
+  updateUsuario(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
+  }
+
+  // DELETE do CRUD
+  deleteUsuario(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, {responseType: `text`});
+  }
+
+
+
 }
