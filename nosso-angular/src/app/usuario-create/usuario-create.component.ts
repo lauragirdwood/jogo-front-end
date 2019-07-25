789029fff23d6fc0f655e84cd1e718001d00f86c
Laura Girdwood <azevedoemello@gmail.com>
2019-07-25 17:02:09 -0300

    add create

--- nosso-angular/src/app/usuario-create/usuario-create.component.ts
+++ nosso-angular/src/app/usuario-create/usuario-create.component.ts
@@ -1,4 +1,7 @@
+import { UsuarioService } from '../usuario.service';
+import { Usuario} from '../usuario';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-create',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioCreateComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario();
+  submitted = false;
+
+  constructor(private usuarioService: UsuarioService,
+    private router: Router) { }
 
   ngOnInit() {
   }
 
+  newUsuario(): void {
+    this.submitted = false;
+    this.usuario = new Usuario();
+  }
+
+  save() {
+    this.usuarioService.createUsuario(this.usuario)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.usuario = new Usuario();
+    this.gotoList();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();    
+  }
+
+  gotoList() {
+    setTimeout(() => {
+    this.router.navigate(['/usuarios']);  
+    }, 2000);    
+  }
 }
