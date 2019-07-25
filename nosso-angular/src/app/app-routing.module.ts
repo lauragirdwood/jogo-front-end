6193acc86a5fddaba971ecfc95d8508a8d8b1c7d
Laura Girdwood <azevedoemello@gmail.com>
2019-07-25 14:31:06 -0300

    editado rotas no app-routing.module.ts

--- nosso-angular/src/app/app-routing.module.ts
+++ nosso-angular/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsuarioListComponent } from './usuario-list/usuario-list.component';
+import { UsuarioUpdateComponent } from './usuario-update/usuario-update.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/usuario', pathMatch: 'full'},
+  {path: 'usuario', component: UsuarioListComponent},
+  {path: 'usuario/:id', component: UsuarioUpdateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
