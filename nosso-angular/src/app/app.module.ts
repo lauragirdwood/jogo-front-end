2a0831908668355ff00da7fc366ecac2b0165691
unknown <Digital@DH-A2-10.dhbrasil.digitalhouse.com>
2019-07-26 11:16:17 -0300

    criados os componentes header e footer

--- nosso-angular/src/app/app.module.ts
+++ nosso-angular/src/app/app.module.ts
@@ -8,13 +8,17 @@ import { UsuarioListComponent } from './usuario-list/usuario-list.component';
 import { UsuarioCreateComponent } from './usuario-create/usuario-create.component';
 import { UsuarioUpdateComponent } from './usuario-update/usuario-update.component';
 import { HttpClientModule } from '../../node_modules/@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsuarioListComponent,
     UsuarioCreateComponent,
-    UsuarioUpdateComponent
+    UsuarioUpdateComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
