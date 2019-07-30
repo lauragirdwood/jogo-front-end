e51befd93ea4830c9e51d7b197ef8e692ef0c634
rebecalopes <rebeca.lopes.141@gmail.com>
2019-07-30 08:55:47 -0300

    adicionado pagina de login no angular

--- nosso-angular/src/app/app-routing.module.ts
+++ nosso-angular/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { UsuarioListComponent } from './usuario-list/usuario-list.component';
 import { UsuarioUpdateComponent } from './usuario-update/usuario-update.component';
 import { UsuarioCreateComponent } from './usuario-create/usuario-create.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'usuarios', component: UsuarioListComponent},
   {path: 'usuario/:id_usuario', component: UsuarioUpdateComponent},
   {path: 'add', component: UsuarioCreateComponent},
-  { path: 'home', component: HomeComponent }
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
